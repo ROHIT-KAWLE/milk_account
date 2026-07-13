4073f969cc356893c5ea639192417f63c31c1c16
ROHIT KAWLE <rohitian9345@gmail.com>
2026-07-13 22:11:59 +0530

    Change fit_columns_on_grid_load to False

--- app.py
+++ app.py
@@ -4472,7 +4472,7 @@ elif menu == "📝 Daily Posting Sheet (Excel)":
             gridOptions=grid_options,
             update_mode="MODEL_CHANGED",
             height=table_height,
-            fit_columns_on_grid_load=True,
+            fit_columns_on_grid_load=False,
             allow_unsafe_jscode=True,
             theme="streamlit",
             use_container_width=True,
